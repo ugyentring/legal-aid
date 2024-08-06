1040c696a785d29c909b4d87c5443e6028143c5b
ugyen.dev <dev.ugyen@gmail.com>
2024-08-06 16:18:07 +0600

    updates

--- api/db/connect.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect(process.env.MONGO_URL || "mongodb://localhost:27017/charges-by-tenants", {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-})
-.then(() => console.log("Database successfully connected ✅"))
-.catch((err) => console.error("Mongodb connection failed ❌", err));
